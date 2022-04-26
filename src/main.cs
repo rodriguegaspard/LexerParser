dd8513e6e380e1af494accf3c0d668ce449360fb
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-04-26 15:36:24 +0200

    First commit

---
+++ src/main.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class HelloWorld {
+    public static void Main() 
+    {
+        Console.WriteLine("--- Lexer Parser ---");
+        Console.WriteLine("Veuillez entrer un calcul.");
+    }
+}
